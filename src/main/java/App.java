c7985608c2623225fc9254cb670b418755f06de9
Seth Zygarlicke <zygster11@gmail.com>
2021-06-08 10:49:42 -0500

    Injected Spring dependencies into project and ran App using ApplicationContext.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,16 +3,16 @@ import learn.solarFarm.data.PanelRepository;
 import learn.solarFarm.domain.PanelService;
 import learn.solarFarm.ui.Controller;
 import learn.solarFarm.ui.View;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 
     public static void main(String[] args) {
 
-        PanelRepository repository = new PanelFileRepository("./data/solar-panels.csv");
-        PanelService service = new PanelService(repository);
-        View view = new View();
+        ApplicationContext container = new ClassPathXmlApplicationContext("dependency-configuration.xml");
 
-        Controller controller = new Controller(view, service);
+        Controller controller = container.getBean(Controller.class);
 
         controller.run();
     }
